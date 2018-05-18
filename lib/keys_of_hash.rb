28688318221424e037cf6405352fc0df9b3e92b7
Matt Maher <mattmaher0@gmail.com>
2018-05-18 20:54:40 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     keys = []
     self.each do |key, value|
-      if arguments[key] == value
+      if arguments[key].include?(value)
         keys << arguments[key]
       end  
     end  
