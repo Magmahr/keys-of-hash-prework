88ce5809a39fb73bad3909e8ee6d60ee5b65dd7b
Matt Maher <mattmaher0@gmail.com>
2018-05-18 20:43:32 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,10 @@
 class Hash
   def keys_of(*arguments)
+    keys = []
     arguments.each do |arg, value|
-      if value == 
+      if Hash.key == value
+        key << arg
+      end  
+    end  
   end
 end
